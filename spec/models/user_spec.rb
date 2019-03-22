577d7aec6fb0d2699c067d0b04eb22f66d6493c0
jun0222 <nextseason6@gmail.com>
2019-03-22 13:49:34 +0900

    Revert "add factory bot"
    This reverts commit 92715d68939d0b130aae97a4a842de51f38b51ff.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it "is invalid without a nickname" do
-     user = build(:user, name: "")
-      user.valid?
-      expect(user.errors[:name]).to include("を入力してください")
+     user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+     user.valid?
+     expect(user.errors[:nickname]).to include("can't be blank")
     end
   end
 end
