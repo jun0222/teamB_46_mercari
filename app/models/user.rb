d9565eb126d45503c69f1cf71750546a902b7952
jun0222 <nextseason6@gmail.com>
2019-03-22 16:12:32 +0900

    add validates users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         validates :nickname,:first_name,:last_name,:first_phonetic,:last_phonetic,:birth_year,:birth_month,:birth_day,:phone_number,:post_address , presence:true
+         validates :email, :phone_number , uniqueness: true
 end
