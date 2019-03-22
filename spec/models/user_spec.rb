a492e00f861bc5c841d839ea53bdbadf7bb7148c
jun0222 <nextseason6@gmail.com>
2019-03-22 14:10:39 +0900

    Change rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it "is invalid without a nickname" do
-     user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+     user = User.new(nickname: "お名前太郎", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
      user.valid?
      expect(user.errors[:nickname]).to include("can't be blank")
     end
