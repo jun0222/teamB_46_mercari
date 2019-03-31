0a4fc321113e537e3afbc6ba9c90176cc15498a9
AkihiroShiraishi <ws.6918rockmusic@icloud.com>
2019-03-31 16:47:12 +0900

    修正２

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20190329070329) do
 ActiveRecord::Schema.define(version: 20190329071955) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
