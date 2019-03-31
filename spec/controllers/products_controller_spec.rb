d8424347ba0e7e6ff173e988556b3455f236dd9f
AkihiroShiraishi <ws.6918rockmusic@icloud.com>
2019-03-31 15:55:00 +0900

    商品詳細ページ

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -8,4 +8,27 @@ describe 'GET #index' do
       expect(response).to render_template :index
     end
   end
+describe 'GET #show' do
+    it "assigns the requested product to @ product" do
+      user = create(:user)
+      product = create(:product)
+      get :show, params: {id: product.id, user_id: product.user_id, id: user.id}
+      expect(assigns(:product)).to eq product
+    end
+    it "renders the :show template" do
+      user = create(:user)
+      product = create(:product)
+      get :show, params: {id: product.id, user_id: product.user_id, id: user.id}
+      expect(response).to render_template :show
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
