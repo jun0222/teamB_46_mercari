5e638c71e9acbfeb5e953f7f09ca9b9be509faf0
jun0222 <nextseason6@gmail.com>
2019-04-03 03:04:08 +0900

    Revert "coment out basic"
    This reverts commit 520c6b53cb3cfcb289d7cea156c263a8b7b9246e.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action :basic_auth, if: :production?
+  before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
@@ -14,8 +14,8 @@ class ApplicationController < ActionController::Base
   end
 
   def basic_auth
-    # authenticate_or_request_with_http_basic do |username, password|
-      # username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
