8b722b48641bdbbd90f2c89550af708219435315
gontabi <kanehisamasaharu@kanehisamasaharu-no-MacBook-Air.local>
2019-04-03 20:28:57 +0900

    product-buy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,13 +12,28 @@ class ProductsController < ApplicationController
       @parent = Tree.find(params[:category])
       @children = Tree.children_of @parent
     end
-    binding.pry
   end
 
   def set_product
      @product = Product.find(params[:id])
      @user = User.find(params[:user_id])
      @products = Product.where(user_id: params[:user_id])
+  end
+
+  def purchase
+    @sold_product = Product.find(params.require(:product_id))
+    @sold_product.sold = 1
+    @sold_product.save
+    Payjp.api_key = PAYJP_SECRET_KEY
+    Payjp::Charge.create(currency: 'jpy', amount: @sold_product.price, card: params['payjp-token'])
+    redirect_to root_path, notice: "支払いが完了しました"
+  end
+
+  def show
+  end
+
+  def conform
+    @product=Product.find(params.require(:id))
   end
 
 end
