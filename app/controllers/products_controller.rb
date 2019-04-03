4bf710b33f58998ea23918fa26e42143a9356b2b
AkihiroShiraishi <ws.6918rockmusic@icloud.com>
2019-04-03 15:46:13 +0900

    商品削除機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,11 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :conform]
+  before_action :set_product, only: [:show, :conform, :myshow]
   def index
-    @products = Product.all
+    @products = Product.order("created_at DESC").page(params[:page]).per(16)
   end
 
   def new
+    @Product = Product.new
     if params[:category] == nil
       @product = Product.new
     else
@@ -12,7 +13,14 @@ class ProductsController < ApplicationController
       @parent = Tree.find(params[:category])
       @children = Tree.children_of @parent
     end
-    binding.pry
+  end
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render "products/new"
+    end
   end
 
   def set_product
@@ -21,4 +29,28 @@ class ProductsController < ApplicationController
      @products = Product.where(user_id: params[:user_id])
   end
 
+  def destroy
+      product = Product.find(params[:id])
+    if product.user_id == current_user.id
+       product.destroy
+       redirect_to myproducts_user_path
+     end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :content,
+      :brand,
+      :size,
+      :state,
+      :price,
+      :shipping,
+      :category,
+      :bearer,
+      :days,
+      :image).merge(user_id: current_user.id)
+    end
 end
