5c83711c6a015f8380c38c45b0d909f3daf7b15c
AkihiroShiraishi <ws.6918rockmusic@icloud.com>
2019-04-05 16:24:16 +0900

    調整

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -8,12 +8,36 @@ crumb :users do
   parent :root
 end
 
+crumb :myproducts_user do
+  link "出品した商品-出品中",  myproducts_user_path
+  parent :users
+end
+
+crumb :profile_user do
+  link "プロフィール", profile_user_path
+  parent :users
+end
+
+crumb :credit_user do
+  link "支払い方法", credit_user_path
+  parent :users
+end
+
+crumb :registration_user do
+  link "本人情報", registration_user_path
+  parent :users
+end
+
+crumb :logout_user do
+  link "ログアウト", logout_user_path
+  parent :users
+end
 # crumb :user do |user|
 #   link "@#{user.nickname}", user_path(user)
 #   parent :users
 # end
 
 crumb :product do |product|
-  link "@#{product.name}", product_path(product)
+  link "#{product.name}", product_path(product)
   parent :root
 end
