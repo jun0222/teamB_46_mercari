32a7ca835a747ac5a17731422018bca00a7deb5c
AkihiroShiraishi <ws.6918rockmusic@icloud.com>
2019-04-05 12:44:51 +0900

    商品編集機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -47,6 +47,17 @@ class ProductsController < ApplicationController
      end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    product = Product.find(params[:id])
+    if product.user_id == current_user.id
+      product.update(product_params)
+    end
+    redirect_to products_path
+  end
 
 # payjp連携用メソッド、ーー 実行後はproduct_buy実行
   def purchase
