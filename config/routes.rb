ece430e53173d85298141d4a20f5117602b91a96
jun0222 <nextseason6@gmail.com>
2019-04-06 20:01:30 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 # コールバック用の記述を追記
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'products#index'
-  resources :products, only: [:index, :new ,:create ,:destroy, :edit, :update]
-  get '/products/search', to:'products#search'
+  resources :products, only: [:index, :new ,:create ,:destroy, :edit, :update] do
+    get '/products/search', to:'products#search'
+  end
 
   resources :users do
     resources :products, only: [:show] do
