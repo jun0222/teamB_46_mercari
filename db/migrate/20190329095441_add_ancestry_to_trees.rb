3aa4d4d843ff09755b3cb4ce2ecacc986ba11493
jun0222 <nextseason6@gmail.com>
2019-04-09 17:07:00 +0900

    go squasher

--- db/migrate/20190329095441_add_ancestry_to_trees.rb
+++
@@ -1,6 +0,0 @@
-class AddAncestryToTrees < ActiveRecord::Migration[5.0]
-  def change
-    add_column :trees, :ancestry, :string
-    add_index :trees, :ancestry
-  end
-end
---
+++ db/migrate/20190404100259_init_schema.rb
@@ -0,0 +1,63 @@
+class InitSchema < ActiveRecord::Migration[5.0]
+  def up
+    create_table "addresses", force: :cascade do |t|
+      t.integer  "prefecture_id"
+      t.string   "city"
+      t.datetime "created_at",    null: false
+      t.datetime "updated_at",    null: false
+    end
+    create_table "products", force: :cascade do |t|
+      t.text     "name",       limit: 65535, null: false
+      t.text     "content",    limit: 65535, null: false
+      t.text     "brand",      limit: 65535
+      t.string   "size"
+      t.string   "state",                    null: false
+      t.boolean  "sold",                     null: false
+      t.datetime "created_at",               null: false
+      t.datetime "updated_at",               null: false
+      t.integer  "price"
+      t.text     "image",      limit: 65535
+      t.string   "shipping",                 null: false
+      t.integer  "user_id"
+      t.text     "detail",     limit: 65535, null: false
+      t.integer  "category",                 null: false
+    end
+    create_table "trees", force: :cascade do |t|
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.string   "ancestry"
+      t.index ["ancestry"], name: "index_trees_on_ancestry", using: :btree
+    end
+    create_table "users", force: :cascade do |t|
+      t.string   "email",                                default: "", null: false
+      t.string   "encrypted_password",                   default: "", null: false
+      t.string   "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
+      t.datetime "created_at",                                        null: false
+      t.datetime "updated_at",                                        null: false
+      t.text     "nickname",               limit: 65535,              null: false
+      t.text     "first_name",             limit: 65535,              null: false
+      t.text     "last_name",              limit: 65535,              null: false
+      t.text     "first_phonetic",         limit: 65535,              null: false
+      t.text     "last_phonetic",          limit: 65535,              null: false
+      t.text     "address",                limit: 65535,              null: false
+      t.integer  "post_address",                                      null: false
+      t.text     "profile_comment",        limit: 65535
+      t.integer  "birth_year",                                        null: false
+      t.integer  "birth_month",                                       null: false
+      t.integer  "birth_day",                                         null: false
+      t.string   "phone_number",                                      null: false
+      t.string   "uid"
+      t.string   "provider"
+      t.string   "token"
+      t.string   "meta"
+      t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+      t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration, "The initial migration is not revertable"
+  end
+end
