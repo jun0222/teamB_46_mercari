18e04d4f1a2a9bd2a82b2b010fe0b5fec3b1b2bc
AkihiroShiraishi <ws.6918rockmusic@icloud.com>
2019-04-11 15:54:55 +0900

    Rspec修正

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 describe ProductsController, type: :controller do
 
+
 describe 'GET #index' do
     it "renders the :index template" do
+       @user = create(:user)
+       sign_in @user
       get :index
       expect(response).to render_template :index
     end
@@ -11,12 +14,15 @@ describe 'GET #index' do
 describe 'GET #show' do
     it "assigns the requested product to @ product" do
       user = create(:user)
+      sign_in user
       product = create(:product)
       get :show, params: {id: product.id, user_id: product.user_id, id: user.id}
       expect(assigns(:product)).to eq product
     end
     it "renders the :show template" do
       user = create(:user)
+
+      sign_in user
       product = create(:product)
       get :show, params: {id: product.id, user_id: product.user_id, id: user.id}
       expect(response).to render_template :show
